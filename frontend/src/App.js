9581a4979d3b45f0617bb36fcb97ed08e816201e
Yuheng Ding <d.yuheng@wustl.edu>
2022-08-03 22:45:17 -0500

    finished setup Auth0 on react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to HackerMeet</h1>
+      <img src="/logo.png" alt="logo" style={{maxWidth: "100px", display: "inline-block"}} />
+      <br />
+      <LoginButton style={{display: "inline-block"}} />
+      <LogoutButton />
     </div>
   );
 }
