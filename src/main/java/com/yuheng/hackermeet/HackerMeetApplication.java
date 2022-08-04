61724539d95946cc2b145dbedcb23f5af64402ec
Yuheng Ding <d.yuheng@wustl.edu>
2022-08-03 21:05:25 -0500

    created Spring Boot Application

---
+++ src/main/java/com/yuheng/hackermeet/HackerMeetApplication.java
@@ -0,0 +1,13 @@
+package com.yuheng.hackermeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackerMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HackerMeetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuheng/hackermeet/HackerMeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuheng.hackermeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerMeetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
