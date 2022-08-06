a92aa117e52fa3b8088c21f02a08c0307cf63937
Yuheng Ding <d.yuheng@wustl.edu>
2022-08-05 22:41:43 -0500

    configured security for h2-console

--- src/main/java/com/yuheng/hackermeet/security/SecurityConfig.java
+++ src/main/java/com/yuheng/hackermeet/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.yuheng.hackermeet.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -37,14 +41,52 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+        http.headers().frameOptions().sameOrigin();
+
         http.authorizeRequests()
-                .mvcMatchers("/").permitAll()
+                .mvcMatchers("/", "/h2-console", "/test").permitAll()
+                .mvcMatchers("/h2-console/**").permitAll()
                 .mvcMatchers("/api").authenticated()
+                .mvcMatchers("/api/data").authenticated()
                 .anyRequest().authenticated()
 //                .mvcMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
                 .and().cors()
                 .and().oauth2ResourceServer().jwt();
+
         return http.build();
     }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+                web
+                        .ignoring()
+                        .antMatchers("/h2-console/**");
+
+    }
+
+    // Deprecated
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web
+//            .ignoring()
+//            .antMatchers("/h2-console/**");
+//    }
+
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.headers().frameOptions().disable();
+//        http.headers().frameOptions().sameOrigin();
+//
+//        http.authorizeRequests()
+//            .mvcMatchers("/", "/h2-console", "/test").permitAll()
+//            .mvcMatchers("/h2-console/**").permitAll()
+//            .mvcMatchers("/api").authenticated()
+//            .mvcMatchers("/api/data").authenticated()
+//            .anyRequest().permitAll()
+//            .and().cors()
+//            .and().oauth2ResourceServer().jwt();
+//    }
 
 }
